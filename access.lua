17be4315f89634848c86aa1970a610844f9aa2fd
Diego Pimentel <diego@siworks.com.br>
2018-01-28 16:07:44 -0200

    Adicionando valor URI baseado nos headers

--- access.lua
+++ access.lua
@@ -3,56 +3,47 @@ local utils = require "kong.tools.utils"
 local responses = require "kong.tools.responses"
 local singletons = require "kong.singletons"
 local req_get_headers = ngx.req.get_headers
+local req_set_headers = ngx.req.set_header
+local print_r = require 'pl.pretty'.dump
+local cjson = require "cjson.safe"
 
 local AUTHENTICATED_USERID = "authenticated_userid"
 
 
 local _M = {}
 
--- local function buildHostHeader(newHost)
---   local u = url.parse(newHost)
---   local hostHeader = u.host
---   if u.port then
---     hostHeader = hostHeader .. ":" .. u.port
---   end
---   return hostHeader
--- end
-
--- local function replaceHost(url, newHost)
---   local pathIndex = url:find('[^/]/[^/]')
-
---   if not pathIndex then
---     if newHost:find('[^/]/[^/]') == nil and newHost:sub(#newHost) ~= "/" then
---       return newHost .. "/"
---     end
-
---     return newHost
---   end
-
---   if newHost:sub(#newHost) == "/" then
---     newHost = newHost:sub(1, -2)
---   end
-
---   local path = url:sub(pathIndex + 1)
---   return newHost .. path
--- end
+-- Exemplo de como gravar regra
+-- ^\/customer\/(\w*)|2:x-authenticated-userid
 
 function _M.execute(config)
-  -- local hostHeader = buildHostHeader(conf.replacement_url) 
-  userid = req_get_headers()["x-authenticated-userid"]
-  newHost = ""
-  for rep, uriRole in ipairs(config.replace.uri) do 
-    
-      elements = splitRole(uriRole,"|")
-      uri = elements[1]
-      replace = elements[2]
-    if(string.find(ngx.var.uri,uri)) then     
-      newHost = string.gsub(ngx.var.uri,uri,userid,2)
-      ngx.log(ngx.NOTICE,newHost)
+  local var_sub = {}   
+  req_set_headers("x-authenticated-userid", 225) -- TEM QUE TIRAR PARA PROD
+  local uriParts = splitRole(ngx.var.uri,"/") 
+  local tempUri = "/"
+  local deuMatch = false
+  for rep, replaces in ipairs(config.replace.uri) do
+    local tmpReplace = splitRole(replaces,"|")
+    local uriFind = tmpReplace[1]    
+    local m = ngx.re.match(ngx.var.uri, uriFind,"i");
+    if m then 
+      deuMatch = true
+      local uriReplace = splitRole(tmpReplace[2],":")
+      local posicao = tonumber(uriReplace[1])      
+      for i = 1, #uriParts, 1 do
+        if i == posicao then
+          if not req_get_headers()[uriReplace[2]] then
+            return responses.send_HTTP_FORBIDDEN("You cannot consume this service")
+          end
+          uriParts[i] = req_get_headers()[uriReplace[2]]
+        end
+        tempUri = tempUri .. uriParts[i] .. "/"
+      end
     end    
-   end
-
-  -- ngx.var.upstream_uri = "/request/" .. newHost  
+  end    
+  if deuMatch then
+    local nova = ngx.re.sub(ngx.var.upstream_uri,ngx.var.uri,"") .. tempUri    
+    ngx.var.upstream_uri = nova
+  end
 end
 
 function splitRole(source, delimiters)
@@ -60,6 +51,6 @@ function splitRole(source, delimiters)
         local pattern = '([^'..delimiters..']+)'
         string.gsub(source, pattern, function(value) elements[#elements + 1] =     value;  end);
         return elements
-  end
+end
 
 return _M
